cf7586c0fe4fb8c8dd3406309099b43510f29cf4
Debiddo <dzheng@terpmail.umd.edu>
2025-04-23 14:10:50 -0700

    added a model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { UserInputComponent } from "./user-input/user-input.component";
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentInput } from './investment-input.model';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,29 @@ import { UserInputComponent } from "./user-input/user-input.component";
   templateUrl: './app.component.html',
   imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  onCalculateInvestmentResults(data:InvestmentInput) {
+    const annualData = [];
+    let investmentValue = data.initialInvestment;
+
+    for (let i = 0; i < data.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (data.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + data.annualInvestment;
+      const totalInterest =
+        investmentValue - data.annualInvestment * year - data.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: data.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
+      });
+    }
+
+    console.log(annualData);
+
+    return annualData;
+  }
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
@@ -16,10 +19,19 @@ export class UserInputComponent {
 
   onSubmit() {
     // Handle form submission logic here
-    console.log('Form submitted');
+    // console.log('Form submitted');
 
-    console.log({ enteredAnnualInvestment: this.enteredAnnualInvestment, enteredDuration: this.enteredDuration, enteredExpectedReturn: this.enteredExpectedReturn, enteredInitialInvestment: this.enteredInitialInvestment });
-
+    // console.log({
+    //   enteredAnnualInvestment: this.enteredAnnualInvestment,
+    //   enteredDuration: this.enteredDuration,
+    //   enteredExpectedReturn: this.enteredExpectedReturn,
+    //   enteredInitialInvestment: this.enteredInitialInvestment,
+    // });
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration,
+    });
   }
-
 }

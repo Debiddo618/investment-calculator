1a07fd312ca0fcbe8883c2fdf815f9cd64fcb61b
Debiddo <dzheng@terpmail.umd.edu>
2025-04-23 12:55:35 -0700

    setup two-way binding

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    // Handle form submission logic here
+    console.log('Form submitted');
+
+    console.log({ enteredAnnualInvestment: this.enteredAnnualInvestment, enteredDuration: this.enteredDuration, enteredExpectedReturn: this.enteredExpectedReturn, enteredInitialInvestment: this.enteredInitialInvestment });
+
+  }
 
 }
